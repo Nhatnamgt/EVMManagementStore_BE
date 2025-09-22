3c4c0e23d2903d8ada4a2bbcdf4f433ce6192736
nam nguyễn <nhatnamgt7@gmail.com>
2025-09-22 15:13:27 +0700

    generic-unit of work

---
+++ EVMManagementStore.Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using PlanyApp.Repository.Base;
+using EVMManagementStore.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVMManagementStore.Repository.UnitOfWork
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        GenericRepository<DealerOrder> DealerOrderRepository { get; }
+        GenericRepository<Inventory> InventoryRepository { get; }
+        GenericRepository<Order> OrderRepository { get; }
+        GenericRepository<Payment> PaymentRepository { get; }
+        GenericRepository<Promotion> PromotionRepository { get; }
+        GenericRepository<PromotionOption> PromotionOptionRepository { get; }
+        GenericRepository<Quotation> QuotationRepository { get; }
+        GenericRepository<Report> ReportRepository { get; }
+        GenericRepository<Role> RoleRepository { get; }
+        GenericRepository<SalesContract> SalesContractRepository { get; }
+        GenericRepository<TestDriveAppointment> TestDriveAppointmentRepository { get; }
+        GenericRepository<Vehicle> VehicleRepository { get; }
+        GenericRepository<User> UserRepository { get; }
+
+        int Save();
+        Task<int> SaveAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+
+    }
+}
