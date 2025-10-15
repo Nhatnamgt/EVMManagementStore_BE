62d54ef2285c1070a5e0990f86dc8c2101551617
nam nguyễn <nhatnamgt7@gmail.com>
2025-10-15 21:10:57 +0700

    get promotion

--- EVMManagementStore/Controllers/PromotionController.cs
+++ EVMManagementStore/Controllers/PromotionController.cs
@@ -1,4 +1,6 @@
 ﻿using EVMManagementStore.Models;
+using EVMManagementStore.Repository.Models;
+using EVMManagementStore.Service.Dealer;
 using EVMManagementStore.Service.DTO;
 using EVMManagementStore.Service.Interface.Dealer;
 using Microsoft.AspNetCore.Authorization;
@@ -15,7 +17,30 @@ namespace EVMManagementStore.Controllers
         public PromotionController(IPromotionService promotionService)
         {
             _promotionService = promotionService;
-        }   
+        }
+
+        [Authorize(Roles = "dealer")]
+        [HttpGet]
+        public async Task<IActionResult> GetVehicle()
+        {
+            var promotion = await _promotionService.GetAllPromotionsAsync();
+            if (promotion == null || !promotion.Any())
+            {
+                return NotFound(ApiResponse<string>.NotFoundResponse("Không có promotion trong danh sách"));
+            }
+            return Ok(ApiResponse<List<PromotionDTO>>.OkResponse(promotion.ToList(), "Lấy danh sách promotion thành công"));
+        }
+        [Authorize(Roles = "dealer")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicleById(int id)
+        {
+            var promotion = await _promotionService.GetPromotionByIdAsync(id);
+            if (promotion == null)
+            {
+                return NotFound(ApiResponse<string>.NotFoundResponse("Không tìm thấy promotion với ID đã cho"));
+            }
+            return Ok(ApiResponse<PromotionDTO>.OkResponse(promotion, "Lấy thông tin promotion thành công"));
+        }
         [Authorize(Roles = "dealer")]
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] PromotionDTO dto)
