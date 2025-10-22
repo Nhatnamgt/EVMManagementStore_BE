07ae6e4dcfde1e730aaf2eb9e01a6e3da521a66f
tungct262 <tungctse182562@fpt.edu.vn>
2025-10-22 08:24:11 +0700

    vehicle

---
+++ EVMManagementStore.Service/Interface/EVM/IEVMVehicleService.cs
@@ -0,0 +1,15 @@
+﻿using EVMManagementStore.Service.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EVMManagementStore.Service.Interface.EVM
+{
+    public interface IEVMVehicleService
+    {
+        Task<IEnumerable<VehicleDTO>> GetAllVehiclesAsync();
+        Task<VehicleDTO?> GetVehicleByIdAsync(int id);
+        Task<VehicleDTO> CreateVehicleAsync(VehicleDTO vehicleDto);
+        Task<VehicleDTO?> UpdateVehicleAsync(int id, VehicleDTO vehicleDto);
+        Task<bool> DeleteVehicleAsync(int id);
+    }
+}
