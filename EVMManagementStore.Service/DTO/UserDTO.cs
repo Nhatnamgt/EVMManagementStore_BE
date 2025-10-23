376cb22fd9286c1df7ca25fbea4f600006533486
tungct262 <tungctse182562@fpt.edu.vn>
2025-10-23 11:24:05 +0700

    user management

---
+++ EVMManagementStore.Service/DTO/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace EVMManagementStore.Service.DTO
+{
+    public class UserDTO
+    {
+        public int UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string CompanyName { get; set; }
+    }
+}
---
+++ EVMManagementStore.Service/Interface/EVM/IUserManagementService.cs
@@ -0,0 +1,20 @@
+﻿using EVMManagementStore.Service.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EVMManagementStore.Service.Interface.EVM
+{
+    public interface IUserManagementService
+    {
+        Task<IEnumerable<UserDTO>> GetAllUsersAsync();
+
+        Task<UserDTO?> GetUserByIdAsync(int id);
+
+
+        Task<UserDTO> CreateUserAsync(UserDTO userDto);
+
+        Task<UserDTO?> UpdateUserAsync(int id, UserDTO userDto);
+
+        Task<bool> DeleteUserAsync(int id);
+    }
+}
