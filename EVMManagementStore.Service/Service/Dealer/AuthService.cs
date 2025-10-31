a7e1993a294b30a54a864bb13408fe73d5e0767a
Nhatnamgt <namnnse182539@fpt.edu.vn>
2025-11-01 00:16:13 +0700

    Add Company_name to LoginResponse

--- EVMManagementStore.Service/Service/Dealer/AuthService.cs
+++ EVMManagementStore.Service/Service/Dealer/AuthService.cs
@@ -48,7 +48,9 @@ namespace EVMManagementStore.Service.Service.Dealer
             return new LoginResponse
             {
                 Token = token,
-                Role = roleName
+                Role = roleName,
+                Company_name = user.CompanyName 
+                
             };
         }
 
